e84a3c4c66e44322005ce2a3fd0017b05b8cbc26
Devo <the.devin.carter@outlook.com>
2018-03-16 18:07:32 -0500

    move increment/decrement functions to seperate file

---
+++ increment.js
@@ -0,0 +1,17 @@
+// increment one function
+function plus(el) {
+    let qty = +el.children[0].innerHTML;
+    qty++;
+    el.children[0].innerHTML = qty;
+    totalCubicFeet();
+}
+
+// decrement one function
+function minus(el) {
+    let qty = +el.children[0].innerHTML;
+    if (qty != 0) {
+        qty--;
+        el.children[0].innerHTML = qty;
+    }
+    totalCubicFeet();
+}
--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-
-// increment one function
-function plus(el) {
-    let qty = +el.children[0].innerHTML;
-    qty ++;
-    el.children[0].innerHTML = qty;
-    totalCubicFeet();
-}
-
-// decrement one function
-function minus(el) {
-    let qty = +el.children[0].innerHTML;
-    if (qty != 0) {
-        qty--;
-        el.children[0].innerHTML = qty;
-    }
-    totalCubicFeet();
-}
-
 // add all divs to window
 document.body.onload = addElements();
 
