885975a49190a9f18ac8e7badc2972e058f6133d
Devo <the.devin.carter@outlook.com>
2018-03-22 23:41:31 -0500

    add divs for addition/subtraction -- remove input buttons

--- create-page.js
+++ create-page.js
@@ -54,6 +54,16 @@ function addElements() {
         newDiv.setAttribute('id', cubeNames[i]);
         newDiv.setAttribute('class', 'flex-child');
 
+        // create div to hold title and count 
+        var plusDiv = document.createElement('DIV');
+        plusDiv.setAttribute('class', 'plusDiv');
+        plusDiv.setAttribute('onclick', 'plus(this.querySelector(".total"))');
+
+        // create hot corner 
+        var hotCorner = document.createElement('DIV');
+        hotCorner.setAttribute('class', 'hotCorner');
+        hotCorner.setAttribute('onclick', 'minus(this.parentNode.querySelector(".total"))');
+
         // create title 
         var h3 = document.createElement('H6');
         h3.innerHTML = cubeNames[i];
@@ -69,29 +79,29 @@ function addElements() {
         p.innerHTML = '0';
 
         // create minus input 
-        var minusInput = document.createElement('INPUT');
-        minusInput.setAttribute('type', 'button');
-        minusInput.setAttribute('value', ' - ');
-        minusInput.setAttribute('class', 'minus');
-        minusInput.setAttribute('onclick', 'minus(this.parentNode)');
+        // var minusInput = document.createElement('INPUT');
+        // minusInput.setAttribute('type', 'button');
+        // minusInput.setAttribute('value', ' - ');
+        // minusInput.setAttribute('class', 'minus');
+        // minusInput.setAttribute('onclick', 'minus(this.parentNode.children[0])');
 
         // create add input
-        var addInput = document.createElement('INPUT');
-        addInput.setAttribute('type', 'button');
-        addInput.setAttribute('value', ' + ');
-        addInput.setAttribute('class', 'plus');
-        addInput.setAttribute('onclick', 'plus(this.parentNode)');
+        // var addInput = document.createElement('INPUT');
+        // addInput.setAttribute('type', 'button');
+        // addInput.setAttribute('value', ' + ');
+        // addInput.setAttribute('class', 'plus');
+        // addInput.setAttribute('onclick', 'plus(this.parentNode)');
 
-        // append title to newDiv
-        newDiv.appendChild(h3);
-        // append qty div to newDiv
-        newDiv.appendChild(qtyDiv);
-        // append p to qtyDiv
+    
+        newDiv.appendChild(plusDiv);
+        plusDiv.appendChild(h3);
+        plusDiv.appendChild(qtyDiv);
         qtyDiv.appendChild(p);
+        newDiv.appendChild(hotCorner);
         // append minus button to qtyDiv
-        qtyDiv.appendChild(minusInput);
+        // qtyDiv.appendChild(minusInput);
         // append plus button to qtyDiv
-        qtyDiv.appendChild(addInput);
+        // qtyDiv.appendChild(addInput);
 
         // append newDiv to containerDiv
         containerDiv.appendChild(newDiv);
