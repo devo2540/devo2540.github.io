51cc9bcd936ff4bf01b3cc2b2a0fd9330d2a7ee9
Devo <the.devin.carter@outlook.com>
2018-03-22 23:40:12 -0500

    change logic to accomodate div changes

--- increment.js
+++ increment.js
@@ -1,18 +1,18 @@
 
 // increment one function
 function plus(el) {
-    let qty = +el.children[0].innerHTML;
+    let qty = +el.innerHTML;
     qty++;
-    el.children[0].innerHTML = qty;
+    el.innerHTML = qty;
     totalCubicFeet();
 }
 
 // decrement one function
 function minus(el) {
-    let qty = +el.children[0].innerHTML;
+    let qty = +el.innerHTML;
     if (qty != 0) {
         qty--;
-        el.children[0].innerHTML = qty;
+        el.innerHTML = qty;
     }
     totalCubicFeet();
 }
